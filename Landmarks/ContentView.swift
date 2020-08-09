e8aae03e0a2e8731f4d8c4b4ec9cc2d7e14bff26
thanawatgulati <thanawatgulatijames@gmail.com>
2020-08-09 14:56:29 +0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Thanawat Gulati on 9/8/2563 BE.
+//  Copyright © 2563 Thanawat Gulati. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
